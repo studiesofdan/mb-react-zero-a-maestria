d535d8a88d3237ed04c05382479a0cb8c903236c
Danilo Santos <artededan@protonmail.ch>
2022-04-28 23:16:46 -0300

    (RZM) Fundamentos: Eventos: Funções nos eventos

--- fundamentos/src/components/Eventos.js
+++ fundamentos/src/components/Eventos.js
@@ -9,6 +9,8 @@ const Eventos = () => {
         <div>
             <div>
                 <button onClick={handleMyEvent}>Clique aqui</button>
+                <br />
+                <button onClick={() => console.log("Este é um botão com função no evento")}>Evento</button>
             </div>
         </div>
     )
