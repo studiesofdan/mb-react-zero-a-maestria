69706361a56bb1f3e3dc1f89eae21f80d72c4a5b
Danilo Santos <artededan@protonmail.ch>
2022-04-28 22:35:42 -0300

    (RZM) Fundamentos: Comentários nos componentes

--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,8 @@
+// Pode ser feito comentários fora da estrutura do JS, pois o componente é um arquivo JS
 const FirstComponent = () => {
     return (
         <div>
+            {/* Comentário na sintaxe do JSX */}
             <h1>Meu primeiro componenteee</h1>
         </div>
     )
