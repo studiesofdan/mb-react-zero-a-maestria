73dab89b363920cacd5cb2fcd0c69882493964ef
Danilo Santos <artededan@protonmail.ch>
2022-04-28 22:57:24 -0300

    (RZM) Fundamentos: Hierarquia de componentes

--- fundamentos/src/components/FirstComponent.js
+++ fundamentos/src/components/FirstComponent.js
@@ -1,9 +1,12 @@
+import HierarquiaComponent from "./HierarquiaComponent"
+
 // Pode ser feito comentários fora da estrutura do JS, pois o componente é um arquivo JS
 const FirstComponent = () => {
     return (
         <div>
             {/* Comentário na sintaxe do JSX */}
             <h1>Meu primeiro componenteee</h1>
+            <HierarquiaComponent />
         </div>
     )
 }
---
+++ fundamentos/src/components/HierarquiaComponent.js
@@ -0,0 +1,9 @@
+const HierarquiaComponent = () => {
+    return (
+        <div>
+            <h3>Estou sendo reaproveitado em vários lugares!!!</h3>
+        </div>
+    )
+}
+
+export default HierarquiaComponent
