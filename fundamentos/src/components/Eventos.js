03f581bf4490ca8962431ab25379fb9d3c7619f8
Danilo Santos <artededan@protonmail.ch>
2022-04-28 23:24:42 -0300

    (RZM) Fundamentos: Função de renderização

--- fundamentos/src/components/Eventos.js
+++ fundamentos/src/components/Eventos.js
@@ -5,6 +5,14 @@ const Eventos = () => {
         console.log("Ativou o evento!")
     };
 
+    const renderizeAlgo = (xanis) => {
+        if (xanis) {
+            return <h1>"G-G-G de gatiiiiiiiiiiiiiiiiiiiiin"</h1>;
+        } else {
+            return <h1>Tuts tuts, quero ver</h1>
+        }
+    }
+
     return (
         <div>
             <div>
@@ -12,6 +20,8 @@ const Eventos = () => {
                 <br />
                 <button onClick={() => console.log("Este é um botão com função no evento")}>Evento</button>
             </div>
+            {renderizeAlgo(true)}
+            {renderizeAlgo(false)}
         </div>
     )
 }
