922adbdb03dbcd23b047c46f3820bf01337130f2
Danilo Santos <artededan@protonmail.ch>
2022-05-04 22:17:34 -0300

    (RZM) Avançando em react: Imagens públicas no React

--- avancando-em-react/src/App.js
+++ avancando-em-react/src/App.js
@@ -6,6 +6,10 @@ function App() {
       <header>
         <h1>Avançando em React</h1>
       </header>
+      <section>
+        {/* Imagem na pasta public */}
+        <img src="/ling-tang-rsD_jv_A8Yo-unsplash.jpg" width="50%" height="50%" alt="Paisagem" />
+      </section>
     </div>
   );
 }
