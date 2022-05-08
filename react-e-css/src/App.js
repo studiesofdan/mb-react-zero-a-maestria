d48dfb882c393fe8666531da34b7fe13bdb8ba6a
Danilo Santos <artededan@protonmail.ch>
2022-05-07 22:34:05 -0300

    (RZM) React e CSS: CSS de componentes

--- react-e-css/src/App.js
+++ react-e-css/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import MeuComponente from './components/ComponentCSS/MeuComponente';
 
 function App() {
   return (
     <div className="App">
       <h1>React e CSS</h1>
+      <MeuComponente />
     </div>
   );
 }
---
+++ react-e-css/src/components/ComponentCSS/MeuComponente.js
@@ -0,0 +1,14 @@
+import './MeuComponente.css';
+
+import React from 'react'
+
+const MeuComponente = () => {
+  return (
+    <div>
+        <h2>Folklore</h2>
+        <h2 className='ui-papaparapou'>Evermore</h2>
+    </div>
+  )
+}
+
+export default MeuComponente
