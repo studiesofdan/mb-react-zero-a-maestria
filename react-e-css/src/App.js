7ca2a21c1f59bb025ddc42c06b6cbed454ed2f15
Danilo Santos <artededan@protonmail.ch>
2022-05-07 22:51:05 -0300

    (RZM) React e CSS: CSS inline dinâmico

--- react-e-css/src/App.js
+++ react-e-css/src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import MeuComponente from './components/ComponentCSS/MeuComponente';
 
 function App() {
+  const gatinnn = 1;
+
   return (
     <div className="App">
       <h1>React e CSS</h1>
+
       <MeuComponente />
+
       <h3 style={{
         color: "rebeccapurple",
       }}>Este elemento foi estilizado de forma inline</h3>
+
+      <h4 style={gatinnn < 10 ? {color: "purple"} : {color: "red"}}> 
+      CSS Dinâmico 
+      </h4>
+      
     </div>
   );
 }
