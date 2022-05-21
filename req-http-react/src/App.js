50b2b3673fb393c2900ddb75fdc6db2967f044ea
Danilo Santos <artededan@protonmail.ch>
2022-05-21 10:29:09 -0300

    (RZM) Requisições HTTP com React: Resgatando dados da API

--- req-http-react/src/App.js
+++ req-http-react/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const url = "http://localhost:3000/musicas";
 
 function App() {
+  const [musicas, setMusicas] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+      const data = await res.json();
+
+      setMusicas(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Lista de músicas</h1>
+      <ul>
+        {musicas.map((musica) => (
+          <li key={musica.id}>
+            {musica.artista} - {musica.faixa}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
