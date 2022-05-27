1fc807e573b5561e5eddbc239d70f4924039024d
Danilo Santos <artededan@protonmail.ch>
2022-05-27 19:36:16 -0300

    (RZM) React Router: Navegação entre páginas

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -5,11 +5,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
 
+// 2 - navegação entre páginas
+import Navbar from './pages/Navbar/Navbar';
+
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
