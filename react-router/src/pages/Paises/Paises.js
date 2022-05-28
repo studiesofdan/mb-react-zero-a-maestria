a540e89e3e285f1cf5011533cb03953d285ca9ae
Danilo Santos <artededan@protonmail.ch>
2022-05-27 21:52:54 -0300

    (RZM) React Router: Carregamento dinâmico de dados

--- react-router/src/pages/Paises/Paises.js
+++ react-router/src/pages/Paises/Paises.js
@@ -1,13 +1,28 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
 
 const Paises = () => {
     // 4 - rota dinâmica
     const { id } = useParams();
     
+    // 5 - carregamento dado individual
+    const url = "http://localhost:3000/paises/" + id;
+    const { data: paises, loading, error } = useFetch(url);
+
+    console.log(paises);
+
     return (
     <>
         <p>ID do país: {id}</p>
+        {error && <p>Erro ao carregr dados</p>}
+        {loading && <p>Carregando...</p>}
+        {paises && (
+            <div>
+                <h1>{paises.pais}</h1>
+                <p>{paises.sigla}</p>
+            </div>
+        )}
     </>
     )
 }
