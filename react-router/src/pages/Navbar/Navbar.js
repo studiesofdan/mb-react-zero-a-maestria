03f08bb3fec30f6a60cb1347b11010ecf1955897
Danilo Santos <artededan@protonmail.ch>
2022-05-27 22:26:27 -0300

    (RZM) React Router: Link ativo

--- react-router/src/pages/Navbar/Navbar.js
+++ react-router/src/pages/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 import "./Navbar.css"
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/paises">Países</Link>
+      {/* Alternativa: className={({ isActive }) => (isActive ? "estaAtivo" : "naoAtivo")} */}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+        <NavLink to="/paises">Países</NavLink>
     </nav>
   )
 }
