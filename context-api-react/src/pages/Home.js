aeb8a693f57e0a8134f94fdf1bd6846b9f9af302
Danilo Santos <artededan@protonmail.ch>
2022-05-30 08:02:23 -0300

    (RZM) Context API do React: Criando um contexto mais complexo

--- context-api-react/src/pages/Home.js
+++ context-api-react/src/pages/Home.js
@@ -3,12 +3,18 @@ import ChangeCounter from '../components/ChangeCounter';
 // 4 - refatorando o hook
 import { useCounterContext } from '../hooks/useCounterContext';
 
+// 5 - context mais complexo
+import { useTitleColorContext } from '../hooks/useTitleColorContext';
+
 const Home = () => {
   const { counter } = useCounterContext();
 
+  // 5 - context mais complexo
+  const { color } = useTitleColorContext();
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1 style={{ color: color }}>Home</h1>
       <p>Valor do contador: {counter}</p>
       {/* 3 - alterando valor */}
       <ChangeCounter />
